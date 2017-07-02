da7cc0196cc1b197cb467e2e6186f953eefa5380
Mitch Guzman <mguzman9294@gmail.com>
2017-07-02 10:02:52 -0700

    initial commit

---
+++ JetWoman/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  JetWoman
+//
+//  Created by Mitch Guzman on 7/2/17.
+//  Copyright © 2017 Mitch Guzman. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
